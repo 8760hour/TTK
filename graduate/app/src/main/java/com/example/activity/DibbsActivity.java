d1cdc1284f00a91249ec6d1fd911a6e55eada686
8760hour <64400666+8760hour@users.noreply.github.com>
2020-08-26 12:51:38 +0900

    push

---
+++ graduate/app/src/main/java/com/example/activity/DibbsActivity.java
@@ -0,0 +1,102 @@
+package com.example.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
+
+import com.android.volley.Response;
+import com.example.Totheking;
+import com.example.graduate.ConnectPHP;
+import com.example.graduate.R;
+import com.example.item.EstimateItemListAdapter;
+import com.example.item.NoneItem;
+import com.example.module.Estimate;
+import com.example.module.RecyclerViewModelBase;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class DibbsActivity extends AppCompatActivity {
+
+    ArrayList<RecyclerViewModelBase> arr;
+    RecyclerView clv;
+    Response.Listener<String> listListener;
+
+    ConnectPHP con;
+
+    public DibbsActivity() {
+        listListener = new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                try {
+                    con.stop();
+                    JSONArray jsonarr = new JSONArray(response);
+                    if (jsonarr.length() > 0) {
+                        for (int i = 0; i < jsonarr.length(); i++) {
+                            JSONObject obj = jsonarr.getJSONObject(i);
+                            arr.add(new Estimate(obj));
+                        }
+                    }
+                    else{
+                        arr.add(new NoneItem());
+                    }
+                    final EstimateItemListAdapter itemsAdapter = new EstimateItemListAdapter(DibbsActivity.this, arr, true);
+                    clv.setAdapter(itemsAdapter);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("찜 목록");
+        setContentView(R.layout.estimate_list);
+        clv = (RecyclerView) findViewById(R.id.clist);
+        arr = new ArrayList<>();
+        clv.setHasFixedSize(true);
+
+        loadBukkit();
+    }
+
+    public void loadBukkit() {
+        Totheking app = (Totheking) this.getApplication();
+        String codes = app.getCodeBukkit();
+        if (codes == null) {
+            arr.add(new NoneItem());
+            final EstimateItemListAdapter itemsAdapter = new EstimateItemListAdapter(DibbsActivity.this, arr, true);
+            clv.setAdapter(itemsAdapter);
+            return;
+        }
+        else {
+            HashMap<String, String> param = new HashMap<>();
+            param.put("codes", codes);
+            con = new ConnectPHP("http://gebalnote.pe.kr/bukkit_list.php", 1, param, listListener, this);
+            con.start();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home: { //toolbar의 back키 눌렀을 때 동작
+                finish();
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
+    }
+}
